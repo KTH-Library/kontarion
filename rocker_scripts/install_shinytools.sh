5f3f7869b2ca2021fb490933bbba3682f75b6b8d
Markus Skyttner <markussk@kth.se>
2022-11-23 17:09:12 +0100

    update shinytools

--- rocker_scripts/install_shinytools.sh
+++ rocker_scripts/install_shinytools.sh
@@ -6,23 +6,22 @@ apt-get -y update && apt-get install -y --no-install-recommends \
 
 # shinycannon, shinyloadtest, shinytest
 
-export TMPDIR=/tmp/cannon 
+export TMPDIR=/tmp/cannon
 
 mkdir -p $TMPDIR
 
 cd $TMPDIR && \
-	wget https://s3.amazonaws.com/rstudio-shinycannon-build/2019-11-22-20:24:21_1.0.0-9b22a92/deb/shinycannon_1.0.0-9b22a92_amd64.deb && \
-	dpkg -i shinycannon_1.0.0-9b22a92_amd64.deb && \
-	rm shinycannon_1.0.0-9b22a92_amd64.deb && cd ..
+	wget -O shinycannon.deb "https://github.com/rstudio/shinycannon/releases/download/v1.1.3/shinycannon_1.1.3-dd43f6b_amd64.deb" && \
+	dpkg -i shinycannon.deb && \
+	rm shinycannon.deb && cd ..
 
-R -e "remotes::install_github('rstudio/shinyloadtest')"
+#R -e "remotes::install_github('rstudio/shinyloadtest')"
 
 install2.r --error --skipinstalled \
+	shinyloadtest \
 	RSelenium \
 	shinytest
 
-# clean up	
+# clean up
 rm -rf $TMPDIR
 rm -rf /var/lib/apt/lists/*
-
-
