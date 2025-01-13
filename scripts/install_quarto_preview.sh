641d467a9cc93b5fc636db0a893f7918c181065e
Markus Skyttner <mskyttner@pm.me>
2025-01-13 11:32:12 +0100

    bump quarto version to v 1.6

--- scripts/install_quarto_preview.sh
+++ scripts/install_quarto_preview.sh
@@ -2,5 +2,7 @@
 set -e
 
 # install quarto
-curl -o quarto-linux-amd64.deb -L https://github.com/quarto-dev/quarto-cli/releases/download/v1.5.57/quarto-1.5.57-linux-amd64.deb
+curl -o quarto-linux-amd64.deb -L \
+	https://github.com/quarto-dev/quarto-cli/releases/download/v1.6.40/quarto-1.6.40-linux-amd64.deb
+
 gdebi --non-interactive quarto-linux-amd64.deb
