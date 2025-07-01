937ad0c096e6e1f06d9154d82d512e0a40e2f794
Agne Larsson <agne.larsson@gmail.com>
2025-07-01 09:41:06 +0200

    Remove downgrade of shiny

--- skouton/scripts/install_bibliotools.sh
+++ skouton/scripts/install_bibliotools.sh
@@ -5,8 +5,6 @@ echo "GITHUB_PAT=$GITHUB_PAT" >> ~/.Renviron
 
 R -e 'update.packages()'
 
-# Downgrade shiny because RI will not work with 1.11.0
-R -e 'remotes::install_version("shiny", "1.10.0")'
 R -e 'remotes::install_github("kth-library/bibliotools", dependencies=TRUE)'
 
 # extra packages, can be removed once included in bibliotools DESCRIPTION file
